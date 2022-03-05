7a32bd91e2399ef62285bdb8c0c9ca09164f15a5
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-03-05 23:43:23 +0200

    init-commit

---
+++ multivariate_linear_regression.m
@@ -0,0 +1,74 @@
+% Multivariate Linear Regression
+
+% Data includes 47 datapoints of housing prices in Portland, Oregon.
+%
+% x=(x1,x2)=(area, number of bedrooms)
+% y= house prices
+
+
+clear all; close all; clc
+
+x = load('mv_regressionx.dat'); 
+y = load('mv_regressiony.dat');
+
+m = length(y);
+
+
+% Add intercept term to x
+x = [ones(m, 1), x];
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% 1. Using 'mvregress' function: "mvregress(x,y)" returns the estimated coefficients for a multivariate normal regression of the d-dimensional responses in Y on the design matrices in X.
+beta1=mvregress(x,y)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% 2. Using Stochastic Gradient Descent with different alphas
+%alpha = [0.01, 0.03, 0.1, 0.3, 1, 1.3]
+% total number of iterations = 100
+
+% Scale features and set them to zero mean
+mu = mean(x);
+sigma = std(x);
+
+x_unscaled = x; % Save unscaled features to calculate the parameters from the normal equation later
+
+% Scaling
+x(:,2) = (x(:,2) - mu(2))./ sigma(2);
+x(:,3) = (x(:,3) - mu(3))./ sigma(3);
+
+% Initialize parameters
+alpha = [0.01, 0.03, 0.1, 0.3, 1, 1.3];
+iters = 100;
+%theta_grad_descent = zeros(size(x(1,:))); % to store the values of theta of the best learning rate
+% Different plotstyle for each alpha value
+plotstyle = {'g', 'r--', 'b--', 'k', 'r', 'b'};
+
+for i = 1:length(alpha)
+    theta = zeros(size(x(1,:)))'; % initialize fitting parameters
+    J = zeros(iters, 1); % initialize J
+    for num_iterations = 1:iters
+        % Calculate J cost
+        J(num_iterations) = (0.5/m).*(x*theta-y)'*(x*theta-y);
+        % The gradient
+        grad=(1/m).*x'*((x*theta)-y);
+        % Updating theta
+        theta=theta-alpha(i).*grad;
+    end
+    % Create a plot for different values of a
+    plot(0:(iters-1), J(1:iters), char(plotstyle(i)), 'LineWidth', 2)
+    hold on
+end
+% Figure
+legend('0.01','0.03','0.1', '0.3', '1', '1.3')
+xlabel('Number of iterations')
+ylabel('Cost J')
+
+format long
+
+% print gradient descent's result
+theta
+
+% Calculate the parameters from the normal equation
+theta_normal = (x_unscaled' * x_unscaled)\x_unscaled' * y
+
---
+++ simple_linear_regression.m
@@ -0,0 +1,86 @@
+% Simple Linear Regression
+
+% The dataset consists of CDC growth statistics for boys
+%
+% x= age (years)
+% y= height (meters)
+%
+
+clear all; close all; clc;
+x = load('linear_regressionx.dat'); y = load('linear_regressiony.dat');
+
+m = length(y); % number of data points = 50
+
+
+% Visualize the training data
+figure;
+plot(x, y, 'o');
+title({'Training Data'})
+ylabel('Y: Height (meters)')
+xlabel('X: Age (years)')
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% 1. Using 'fitlm' function: "fitlm(x,y)" uses intercept by default 2 
+a1 = fitlm(x,y);
+W=a1.Coefficients{:,1}
+W
+ 
+% Visualize results
+figure
+hold on
+scatter(x,y)
+plot(x, W(1)+W(2)*x, '-')
+ylabel('Y: Height (meters)')
+xlabel('X: Age (years)')
+title({'Simple Linear Regression using fitlm(x,y) function'});
+hold off
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% 2. Using 'regress' function: "regress(y,x)" uses no intercept by default. 
+% We could add intercept by adding "ones" matrix. 
+
+X = [ones(size(x)) x]
+a2 = regress(y,X);
+a2
+
+% Visualize results
+figure
+hold on
+scatter(x,y)
+plot(x, a2(1)+a2(2)*x, '-')
+ylabel('Y: Height (meters)')
+xlabel('X: Age (years)')
+title({'Simple Linear Regression using regress(x,y) function'});
+hold off
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% 3. Using Stochastic Gradient Descent
+% alpha=0.07
+% total number of iterations=1500
+
+theta = 0;
+iterations = 1500;
+alpha = 0.07;
+x2 = X;
+for num_iterations = 1:iterations
+   
+    grad = (1/m).* x2' * ((x2 * theta) - y);
+    theta = theta - alpha .* grad;
+    
+end
+
+theta
+
+% Visualize results
+figure
+hold on
+scatter(x,y)
+plot(x2(:,2), x2*theta, '-')
+ylabel('Y: Height (meters)')
+xlabel('X: Age (years)')
+title({'Simple Linear Regression using SGD method (iter=150)'});
+hold off
+
+
+
